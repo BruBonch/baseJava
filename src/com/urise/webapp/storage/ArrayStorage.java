5e45f99d4c13dba557a9adde724d8f2876b3cd47
Bru Bonch <Wexller35@gmail.com>
2024-11-19 18:55:39 +0300

    Рефакторинг - количество резюме вынесено в константу - переписана логика сохранения резюме - переписана логика метода getResumeIndex

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,65 +2,60 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final int STORAGE_LIMIT = 10_000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size;
 
     public void clear() {
-        for (int i = size - 1; i >= 0; i--) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void save(Resume resume) {
-        int resumeIndex = getResumeIndex(resume.getUuid());
-        if (resumeIndex == -1) {
-            if (size == storage.length) {
-                System.out.println("Error. No free space for resumes in the storage");
-            } else {
-                storage[size] = resume;
-                size++;
-            }
-        } else {
+        int index = getResumeIndex(resume.getUuid());
+        if (size == storage.length) {
+            System.out.println("Error. No free space for resumes in the storage");
+        } else if (index != -1) {
             System.out.println("Error. Resume with ID - " + resume.getUuid() + " already create");
+        } else {
+            storage[size] = resume;
+            size++;
         }
     }
 
     public void update(Resume resume) {
-        int resumeIndex = getResumeIndex(resume.getUuid());
-        if (resumeIndex == -1) {
+        int index = getResumeIndex(resume.getUuid());
+        if (index == -1) {
             System.out.println("Error. Resume with ID - " + resume.getUuid() + " not found");
         } else {
-            storage[resumeIndex] = resume;
+            storage[index] = resume;
         }
 
     }
 
-    public Resume[] getStorage() {
-        return storage;
-    }
-
     public Resume get(String uuid) {
         Resume resume = null;
-        int resumeIndex = getResumeIndex(uuid);
-        if (resumeIndex == -1) {
+        int index = getResumeIndex(uuid);
+        if (index == -1) {
             System.out.println("Error. Resume with ID - " + uuid + " not found");
         } else {
-            resume = storage[resumeIndex];
+            resume = storage[index];
         }
         return resume;
     }
 
     public void delete(String uuid) {
-        int resumeIndex = getResumeIndex(uuid);
-        if (resumeIndex == -1) {
+        int index = getResumeIndex(uuid);
+        if (index == -1) {
             System.out.println("Error. Resume with ID - " + uuid + " not found");
         } else {
-            storage[resumeIndex] = storage[size - 1];
+            storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
         }
@@ -71,11 +66,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] resumes = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            resumes[i] = storage[i];
-        }
-        return resumes;
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
@@ -83,13 +74,12 @@ public class ArrayStorage {
     }
 
     private int getResumeIndex(String uuid) {
-        int result = -1;
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
-                result = i;
-                break;
+                return i;
+
             }
         }
-        return result;
+        return -1;
     }
 }
