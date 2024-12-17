71d409f6310daa7ece7a5e437eb4a5f20f12b0e1
Bru Bonch <Wexller35@gmail.com>
2024-12-17 13:20:38 +0300

    Описывает начало шаблонного метода

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,11 +7,7 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage {
-    private final int STORAGE_LIMIT = 10_000;
-    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size;
-
+public class ArrayStorage extends AbstractArrayStorage {
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
@@ -20,7 +16,7 @@ public class ArrayStorage implements Storage {
     public void save(Resume resume) {
         if (size >= STORAGE_LIMIT) {
             System.out.println("Error. No free space for resumes in the storage");
-        } else if (getResumeIndex(resume.getUuid()) != -1) {
+        } else if (getIndex(resume.getUuid()) != -1) {
             System.out.println("Error. Resume with ID - " + resume.getUuid() + " already create");
         } else {
             storage[size] = resume;
@@ -29,7 +25,7 @@ public class ArrayStorage implements Storage {
     }
 
     public void update(Resume resume) {
-        int index = getResumeIndex(resume.getUuid());
+        int index = getIndex(resume.getUuid());
         if (index == -1) {
             System.out.println("Error. Resume with ID - " + resume.getUuid() + " not found");
         } else {
@@ -38,18 +34,8 @@ public class ArrayStorage implements Storage {
 
     }
 
-    public Resume get(String uuid) {
-        int index = getResumeIndex(uuid);
-        if (index == -1) {
-            System.out.println("Error. Resume with ID - " + uuid + " not found");
-        } else {
-            return storage[index];
-        }
-        return null;
-    }
-
     public void delete(String uuid) {
-        int index = getResumeIndex(uuid);
+        int index = getIndex(uuid);
         if (index == -1) {
             System.out.println("Error. Resume with ID - " + uuid + " not found");
         } else {
@@ -67,11 +53,8 @@ public class ArrayStorage implements Storage {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
-    public int size() {
-        return size;
-    }
 
-    private int getResumeIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
